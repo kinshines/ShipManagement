b4e78478ada3280d5aa0fbb8329f33d3ba097194
kinshines <1176807665@qq.com>
2018-07-26 23:54:36 +0800

    view

---
+++ Ship.Infrastructure/TagHelpers/AjaxSuccessTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ship.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("form", Attributes = "ajax-success")]
+    public class AjaxSuccessTagHelper : TagHelper
+    {
+        public string AjaxSuccess { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("data-ajax", "true");
+            output.Attributes.SetAttribute("data-ajax-method", "post");
+            output.Attributes.SetAttribute("data-ajax-mode", "replace");
+            output.Attributes.SetAttribute("data-ajax-success", AjaxSuccess);
+        }
+    }
+}
