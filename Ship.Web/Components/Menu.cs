9ddf6515bf148fb5e145ae3a1167a2e3e78d5a40
kinshines <1176807665@qq.com>
2018-07-29 21:35:19 +0800

    menu

---
+++ Ship.Web/Components/Menu.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ship.Web.Components
+{
+    public class Menu: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ship.Web/Components/PreFooter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ship.Web.Components
+{
+    public class PreFooter: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
