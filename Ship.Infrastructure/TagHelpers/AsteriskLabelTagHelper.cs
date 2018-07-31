a0baca80a3eb0c522039d5e581ef6de9c1553fc2
kinshines <1176807665@qq.com>
2018-08-01 00:06:41 +0800

    asterisk

---
+++ Ship.Infrastructure/TagHelpers/AsteriskLabelTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ship.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("label", Attributes = "asterisk")]
+    public class AsteriskLabelTagHelper: TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.PostContent.SetHtmlContent("<span class='required' aria-required='true'>*</span>");
+        }
+    }
+}
